ca0edc890917b3995241c93c999eaa3d8d9e4d26
ourandream <ouranhuang@outlook.com>
2024-03-03 16:28:20 +0800

    feat: add spreadsheet

--- frontend/pages/Documents.vue
+++ frontend/pages/Documents.vue
@@ -2,29 +2,48 @@
   <div class="flex py-2 px-2">
     <USelect v-model="currentProject" :options="projects" />
     <USelect
-      v-model="currentDocument"
+      v-model="currentDocumentType"
       :options="projectDocuments"
+      option-attribute="name"
+      value-attribute="type"
       class="ml-2"
     />
   </div>
-  <MdEditor v-model="text" />
+  <MdEditor v-model="text" v-if="currentDocumentType === 'md'" />
+  <div
+    id="x-spreadsheet-demo"
+    v-else-if="currentDocumentType === 'spreadsheet'"
+  ></div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { MdEditor } from "md-editor-v3";
 import "md-editor-v3/lib/style.css";
-
 const text = ref("# Hello Editor");
 
 const projects = ref(["项目1", "项目2"]);
 const projectDocumentMap = {
-  项目1: ["概要设计", "详细设计"],
-  项目2: ["文档1"],
+  项目1: [
+    { name: "概要设计", type: "md" },
+    { name: "详细设计", type: "md" },
+  ],
+  项目2: [{ name: "文档1", type: "spreadsheet" }],
 };
 const currentProject = ref<keyof typeof projectDocumentMap>("项目1");
 const projectDocuments = computed(() => {
   return projectDocumentMap[currentProject.value];
 });
-const currentDocument = ref("");
+const currentDocumentType = ref("md");
+
+watch(currentDocumentType, async (newType) => {
+  if (newType === "spreadsheet") {
+    const { default: Spreadsheet } = await import("x-data-spreadsheet");
+    const s = new Spreadsheet("#x-spreadsheet-demo")
+      .loadData({}) // load data
+      .change((data) => {
+        // save data to db
+      });
+  }
+});
 </script>
